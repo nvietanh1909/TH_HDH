4957b47c131c3108e73a41b53bc7d812d62137a9
nvietanh1909 <nvietanh.190904@example.com>
2023-11-18 07:14:03 +0700

    18/11/2023

---
+++ TH8/22DH110144_NguyenVietAnh/22DH110144_NguyenVietAnh/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _22DH110144_NguyenVietAnh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbDrives = new System.Windows.Forms.ComboBox();
+            this.tvFolders = new System.Windows.Forms.TreeView();
+            this.dgvFiles = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn ổ đĩa:";
+            // 
+            // cbDrives
+            // 
+            this.cbDrives.FormattingEnabled = true;
+            this.cbDrives.Location = new System.Drawing.Point(104, 16);
+            this.cbDrives.Name = "cbDrives";
+            this.cbDrives.Size = new System.Drawing.Size(147, 24);
+            this.cbDrives.TabIndex = 1;
+            this.cbDrives.SelectedIndexChanged += new System.EventHandler(this.cbDrives_SelectedIndexChanged);
+            // 
+            // tvFolders
+            // 
+            this.tvFolders.Location = new System.Drawing.Point(30, 65);
+            this.tvFolders.Name = "tvFolders";
+            this.tvFolders.Size = new System.Drawing.Size(268, 374);
+            this.tvFolders.TabIndex = 2;
+            this.tvFolders.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvFolders_NodeMouseClick);
+            this.tvFolders.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvFolders_NodeMouseDoubleClick);
+            // 
+            // dgvFiles
+            // 
+            this.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFiles.Location = new System.Drawing.Point(304, 65);
+            this.dgvFiles.Name = "dgvFiles";
+            this.dgvFiles.RowHeadersWidth = 51;
+            this.dgvFiles.RowTemplate.Height = 24;
+            this.dgvFiles.Size = new System.Drawing.Size(484, 373);
+            this.dgvFiles.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvFiles);
+            this.Controls.Add(this.tvFolders);
+            this.Controls.Add(this.cbDrives);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbDrives;
+        private System.Windows.Forms.TreeView tvFolders;
+        private System.Windows.Forms.DataGridView dgvFiles;
+    }
+}
+
---
+++ TH8/22DH110144_NguyenVietAnh/22DH110144_NguyenVietAnh/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22DH110144_NguyenVietAnh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            cbDrives.Items.Clear();
+            foreach(DriveInfo d in allDrives)
+            {
+                cbDrives.Items.Add(d.Name);
+            }
+            cbDrives.SelectedIndex = 0;
+            dgvFiles.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dgvFiles.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dgvFiles.GridColor = Color.Black;
+            dgvFiles.ColumnCount = 3;
+            dgvFiles.Columns[0].Name = "Tên File";
+            dgvFiles.Columns[1].Name = "Kích thước";
+            dgvFiles.Columns[2].Name = "Cập nhật lần cuối lúc";
+            dgvFiles.Font = new Font("Tahoma", 10f);
+            dgvFiles.Name = "dgvFiles";
+            dgvFiles.AutoResizeColumns();
+            dgvFiles.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvFiles.MultiSelect = true;
+            dgvFiles.AllowUserToResizeColumns = true;
+
+
+        }
+
+        private void cbDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String sdrv = cbDrives.SelectedItem.ToString();
+            tvFolders.Nodes.Clear();
+            TreeNode FdNode = new TreeNode();
+            FdNode.Text = sdrv;
+            tvFolders.Nodes.Add(FdNode);
+            Populate(FdNode, sdrv);
+            FdNode.Expand();
+            tvFolders.Refresh();
+        }
+        private void Populate(TreeNode FdNode, String sdir)
+        {
+            string[] sdlistw = Directory.GetDirectories(sdir);
+            TreeNode ChildNode;
+            FdNode.Nodes.Clear();
+            foreach(string subdir in sdlistw)
+            {
+                string[] lstStr = subdir.Split('\\');
+                ChildNode = new TreeNode();
+                ChildNode.Text = lstStr[lstStr.Length - 1];
+                ChildNode.Nodes.Add(new TreeNode());
+                FdNode.Nodes.Add(ChildNode);
+            }
+        }
+
+        private void tvFolders_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if(e.X > e.Node.Bounds.Left)
+            {
+                DisplayFiles(getDir(e.Node));
+            }
+            else
+            {
+                tvFolders_NodeMouseClick(sender, e);
+            }
+        }
+        private void DisplayFiles(String sdir)
+        {
+            dgvFiles.Rows.Clear();
+            string[] flist = Directory.GetFiles(sdir);
+            String[] sbuf = new String[3];
+            String[] buf;
+            foreach(string fname in flist)
+            {
+                FileInfo fi = new FileInfo(fname);
+                sbuf[1] = fi.Length.ToString();
+                sbuf[2] = fi.LastWriteTime.ToString();
+                buf = fname.Split('\\');
+                sbuf[0] = buf[buf.Length - 1];
+                dgvFiles.Rows.Add(sbuf);
+            }
+        }
+        private String getDir(TreeNode node)
+        {
+            String kq = node.Text;
+            while(node.Parent != null)
+            {
+                node = node.Parent;
+                if (node.Parent != null) kq = node.Text + '\\' + kq;
+                else kq = node.Text + kq;
+            }
+            return kq;
+        }
+
+        private void tvFolders_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode node = e.Node;
+            if (node.NextNode != null && node.IsExpanded == false) node.Collapse();
+            else
+            {
+                String sdir = getDir(node);
+                Populate(node, sdir);
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int cx = ClientSize.Width;
+            int cy = ClientSize.Height;
+            int top = tvFolders.Top;
+            int left = tvFolders.Left;
+            tvFolders.Size = new Size(cx / 3 - left, cy - 8 - top);
+            dgvFiles.Location = new Point(cx / 3, top);
+            dgvFiles.Size = new Size(2 * cx / 3 - 8, cy - 8 - top);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
